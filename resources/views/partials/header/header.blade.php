6c6a839719d7b5854da896e93aafd86a00acf761
Trevor Thompson <tsquare1717@gmail.com>
2020-03-17 21:05:33 -0400

    Fix escaping generated html in blade templates.

--- resources/views/partials/header/header.blade.php
+++ resources/views/partials/header/header.blade.php
@@ -1,11 +1,11 @@
 <header id="header">
     <div id="branding">
         <div id="site-title">
-            {{ is_front_page() || is_home() ? '<h1>' : '' }}
+            {!! is_front_page() || is_home() ? '<h1>' : '' !!}
             <a href="{{ esc_url(home_url('/')) }}" title="{{ esc_html(get_bloginfo('name')) }}" rel="home">
                 {{ esc_html(get_bloginfo('name')) }}
             </a>
-            {{ is_front_page() || is_home() ? '</h1>' : '' }}
+            {!! is_front_page() || is_home() ? '</h1>' : '' !!}
         </div>
         <div id="site-description">{{ get_bloginfo('description') }}</div>
     </div>
--- resources/views/wp_template/entry.blade.php
+++ resources/views/wp_template/entry.blade.php
@@ -1,11 +1,11 @@
 <article id="post-{{ get_the_ID() }}" @php(post_class())>
     <header>
-        {{ is_singular() ? '<h1 class="entry-title">' : '<h2 class="entry-title">' }}
+        {!! is_singular() ? '<h1 class="entry-title">' : '<h2 class="entry-title">' !!}
 
         <a href="{{ get_the_permalink() }}" title="{{ the_title_attribute() }}" rel="bookmark">
-            {{ the_title() }}
+            {!! the_title() !!}
         </a>
 
-        {{ is_singular() ? '</h1>' : '</h2>' }} {{ get_edit_post_link() }}
+        {!! is_singular() ? '</h1>' : '</h2>' !!} @php(edit_post_link())
     </header>
 </article>
