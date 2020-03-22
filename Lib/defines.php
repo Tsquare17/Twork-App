bae18aaf96ac70d6e70be45d8fc11b772a82e54c
Trevor Thompson <tsquare1717@gmail.com>
2020-03-21 23:38:13 -0400

    Add img dir.

--- Lib/defines.php
+++ Lib/defines.php
@@ -5,3 +5,4 @@ define('TWORK_PATH', dirname(__DIR__, 1));
 define('TWORK_CLI_PATH', __DIR__ . '/Cli');
 define('TWORK_JS_URL', get_template_directory_uri() . '/resources/assets/js/dist');
 define('TWORK_CSS_URL', get_template_directory_uri() . '/resources/assets/css/dist');
+define('TWORK_IMG_URL', get_template_directory_uri() . '/resources/assets/img');
