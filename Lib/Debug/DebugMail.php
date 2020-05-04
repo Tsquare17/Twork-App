c917de201184d8c78d2fcf7a9bca0b2845f53e9a
Trevor Thompson <tsquare1717@gmail.com>
2020-05-04 18:49:28 -0400

    Add docblocks.

--- Lib/Debug/DebugMail.php
+++ Lib/Debug/DebugMail.php
@@ -2,13 +2,26 @@
 
 namespace Twork\Debug;
 
+/**
+ * Class DebugMail
+ * @package Twork\Debug
+ */
 class DebugMail
 {
+    /**
+     * DebugMail constructor.
+     */
     public function __construct()
     {
         add_action('phpmailer_init', [$this, 'helo']);
     }
 
+    /**
+     * Override the mail settings to be intercepted by HELO.
+     * https://usehelo.com/
+     *
+     * @param $phpmailer
+     */
     public function helo($phpmailer): void
     {
         $phpmailer->isSMTP();
