d7f499b0f58b279563914906fda3d7fb082b0271
Trevor Thompson <tsquare1717@gmail.com>
2020-05-09 12:04:23 -0400

    Add ability to use SMTP.

--- Lib/Setup.php
+++ Lib/Setup.php
@@ -2,6 +2,8 @@
 
 namespace Twork;
 
+use Dotenv\Dotenv;
+
 /**
  * Class Setup
  * @package Twork
@@ -13,6 +15,8 @@ class Setup
      */
     public function __construct()
     {
+        $this->getEnv();
+
         add_action('wp_enqueue_scripts', [$this, 'enqueueGlobalScripts']);
         add_action('wp_enqueue_scripts', [$this, 'enqueueGlobalStyles']);
 
@@ -34,6 +38,34 @@ class Setup
     {
         wp_enqueue_style('twork-stylesheet', get_stylesheet_uri(), null, TWORK_VERSION);
         wp_enqueue_style('twork-css', TWORK_CSS_URL . '/twork.min.css', null, TWORK_VERSION);
+    }
+
+    /**
+     * Load env and setup environment.
+     */
+    public function getEnv(): void
+    {
+        $config = Dotenv::createImmutable(TWORK_PATH);
+        $config->load();
+
+        if (getenv('SMTP')) {
+            add_filter('phpmailer_init', [$this, 'useSmtp']);
+        }
+    }
+
+    /**
+     * Set SMTP configuration.
+     *
+     * @param $mailer
+     */
+    public function useSmtp($mailer): void
+    {
+        $mailer->isSMTP();
+        $mailer->Host = getenv('SMTP_HOST');
+        $mailer->SMTPAuth = true;
+        $mailer->Port = getenv('SMTP_PORT');
+        $mailer->Username = getenv('SMTP_USER');
+        $mailer->Password = getenv('SMTP_PASS');
     }
 
     /**
