3ef3a3040c926492e109dc9088c39a7e3c7ddc86
Trevor Thompson <tsquare1717@gmail.com>
2020-11-18 22:08:32 -0500

    Change theme constants back.

--- Theme/defines.php
+++ Theme/defines.php
@@ -1,9 +1,8 @@
 <?php
 
-define('TWORK_APP_CONST', 'TWORK');
-define(TWORK_APP_CONST . '_VERSION', '1.0.0');
-define(TWORK_APP_CONST . '_PATH', dirname(__DIR__, 1));
-define(TWORK_APP_CONST . '_VIEWS', get_template_directory_uri() . '/resources/views');
-define(TWORK_APP_CONST . '_JS_URL', get_template_directory_uri() . '/resources/assets/js/dist');
-define(TWORK_APP_CONST . '_CSS_URL', get_template_directory_uri() . '/resources/assets/css/dist');
-define(TWORK_APP_CONST . '_IMG_URL', get_template_directory_uri() . '/resources/assets/img');
+define('TWORK_VERSION', '1.0.0');
+define('TWORK_PATH', dirname(__DIR__, 1));
+define('TWORK_VIEWS', get_template_directory_uri() . '/resources/views');
+define('TWORK_JS_URL', get_template_directory_uri() . '/resources/assets/js/dist');
+define('TWORK_CSS_URL', get_template_directory_uri() . '/resources/assets/css/dist');
+define('TWORK_IMG_URL', get_template_directory_uri() . '/resources/assets/img');
